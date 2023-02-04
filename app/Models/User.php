11679f5866181efce3b2499817d3939e7d0df6c5
tsunodamakoto <tunoda2227@gmail.com>
2023-02-04 16:14:47 +0900

    コメント機能追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Models\Post');
     }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('/post', 'PostController');
+
+Route::post('/post/comment/store', 'CommentController@store')->name('comment.store');
