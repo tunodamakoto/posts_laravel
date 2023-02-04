9f11e7a30434b92500e4d50e5a7f99b17d8fcbef
tsunodamakoto <tunoda2227@gmail.com>
2023-02-04 12:24:27 +0900

    投稿一覧

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,7 +82,16 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-4 col-lg-3">
+                        @include('layouts.sidebar')
+                    </div>
+                    <div class="col-12 col-md-8  col-lg-9">                 
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
         </main>
     </div>
 </body>
